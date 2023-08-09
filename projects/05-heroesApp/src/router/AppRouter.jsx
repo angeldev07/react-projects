2efca528631ad49bf9b8c6ca8981d12188bc45d7
angeldev07 <angelgaran20@gmail.com>
2023-08-09 18:29:55 -0500

    feat(05-Heroesapp): Created inititial structure

---
+++ projects/05-heroesApp/src/router/AppRouter.jsx
@@ -0,0 +1,53 @@
+import { createBrowserRouter, Navigate, RouterProvider } from 'react-router-dom'
+import { Marvel } from '../heroes/pages/Marvel'
+import { DC } from '../heroes/pages/DC'
+import { HeroesApp } from '../HeroesApp'
+import { Login } from '../auth/pages/Login'
+import { Hero } from '../heroes/pages/Hero'
+import { Search } from '../heroes/pages/Search'
+
+const router = createBrowserRouter([
+	{
+		path: '',
+		element: <HeroesApp />,
+		children: [
+			{
+				path: 'marvel',
+				element: <Marvel />,
+			},
+			{
+				path: 'dc',
+				element: <DC />,
+			},
+			{
+				path: 'hero',
+				element: <Hero />,
+			},
+			{
+				path: 'search',
+				element: <Search/>,
+			},
+			{
+				path: '',
+				element: <Navigate to="/marvel" />,
+			},
+		],
+	},
+	{
+		path: 'auth',
+		children: [
+			{
+				path: 'login',
+				element: <Login />,
+			},
+			{
+				path: '',
+				element: <Navigate to="/auth/login" />,
+			}
+		]
+	}
+])
+
+export const AppRouter = () => {
+	return <RouterProvider router={router}></RouterProvider>
+}
