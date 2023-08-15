356d856dc0fa790a91fdfb673fb233b431f786b5
angeldev07 <angelgaran20@gmail.com>
2023-08-15 11:01:40 -0500

    chore(06-countryApp): Inicializate and configurate router, tailwind, and more.

---
+++ projects/06-countriesApp/src/router/main-router.jsx
@@ -0,0 +1,17 @@
+import { createBrowserRouter } from 'react-router-dom'
+import { CountryApp } from '../CountryApp'
+
+export const router = createBrowserRouter([
+	{
+		path: '',
+		element: <CountryApp />,
+	},
+	{
+		path: 'country/:codeCountry',
+		element: <h1> Pagina de detalle</h1>,
+	},
+	{
+		path: '*',
+		element: <h1> 404</h1>,
+	},
+])
